c4b30cf47e5b2da1130ed6b0ad672d9bc0b5677a
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2020-11-05 15:52:10 -0500

    Initial Commit

---
+++ chatApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  chatApp
+//
+//  Created by Abhinav Bhardwaj on 2020-11-05.
+//  Copyright © 2020 Abhinav Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chatApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  chatApp
+//
+//  Created by Abhinav Bhardwaj on 2020-11-05.
+//  Copyright © 2020 Abhinav Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
